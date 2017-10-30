4ba82d2e9ad34121e891648b7e68aa003ef0f025
Nicolai <nicolai_m404@hotmail.com>
2017-10-30 15:56:57 +0100

    Started user creation

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
 
-var MOVIE_COLLECTION = "Movies";
+/*
+ *In this file we establish a connection to our database.
+ * We also establish our routes
+ */
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
 
-var app = express();
+const MOVIE_COLLECTION = "Movies";
+let USER_COLLECTION = "Users";
+let MYMOVIES_COLLECTION = "MyMovies";
+
+const app = express();
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+const router = express.Router();
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in our app.
+let db;
 
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect("mongodb://localhost:27017/movieDB", function (err, database) {
@@ -23,13 +31,13 @@ mongodb.MongoClient.connect("mongodb://localhost:27017/movieDB", function (err, 
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
+  const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
     console.log("App now running on port", port);
   });
 });
 
-// CONTACTS API ROUTES BELOW
+// MOVIE and USER API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -38,7 +46,7 @@ function handleError(res, reason, message, code) {
 }
 
 /*  "/api/Movies"
- *    GET: finds all movies
+ *   GET: finds all movies
  */
 
 app.get("/api/Movies", function(req, res) {
@@ -51,7 +59,9 @@ app.get("/api/Movies", function(req, res) {
   });
 });
 
-//Find info on one specific movie
+/*
+ * Find info on one specific movie by id
+ */
 app.get("/api/Movies/:id", function(req, res) {
   db.collection(MOVIE_COLLECTION).findOne({ Rank: parseInt(req.params.id) }, function(err, doc) {
     if (err) {
@@ -61,5 +71,30 @@ app.get("/api/Movies/:id", function(req, res) {
     }
   });
 });
+
+/*
+ *Some type of inserting movies into MyPage
+
+app.post("/api/Movies/")*/
+
+
+/*
+ * Some type of user inserting and deleting
+ */
+router.post("/register", function(req,res) {
+  let newUser = new User({
+    name: req.body.name,
+    username: req.body.username
+  });
+
+  User.addUser(newUser, function(req,res)
+  if (err) {
+    handleError(res,err.message, "Failed to register user");
+  } else {
+    db.collection(USER_COLLECTION).insertOne()
+  }
+});
+
+module.exports = router;
 
 
